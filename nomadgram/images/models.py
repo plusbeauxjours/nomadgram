2d85ca3cd153aa3bb21af04e097a742a5557229d
plusbeauxjours <plusbeauxjours@gmail.com>
2017-11-02 03:13:55 +0900

    following & followers, admin customizing

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -14,16 +14,24 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User)
+    creator = models.ForeignKey(user_models.User, null=True) 
+
+    def __str__(self):
+        return 'Location: {} - Caption: {} - Creator: {}'.format(self.location, self.caption, self.creator)
 
 class Comment(TimeStampedModel):
 
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User)
-    image = models.ForeignKey(Image)
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+    
+    def __str__(self):
+        return self.message
+    
 class Like(TimeStampedModel):
 
-    creator = models.ForeignKey(user_models.User)
-    image = models.ForeignKey(Image)
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
 
-
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
