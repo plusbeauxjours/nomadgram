85133b37553e0d420f20c37e0047cb591a3dc284
plusbeauxjours <plusbeauxjours@gmail.com>
2017-11-13 04:33:30 +0900

    Search users by username

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -19,18 +19,23 @@ urlpatterns = [
         name='unfollow_user',
     ),
     url(
-        regex=r'^(?P<username>\w+)/$',
-        view=views.UserProfile.as_view(),
-        name='user_profile',
-    ),
-    url(
         regex=r'^(?P<username>\w+)/followers/$',
         view=views.UserFollowers.as_view(),
         name='user_follwers',
     ),
     url(
-    regex=r'^(?P<username>\w+)/following/$',
-    view=views.UserFollowing.as_view(),
-    name='user_follwing',
+        regex=r'^(?P<username>\w+)/following/$',
+        view=views.UserFollowing.as_view(),
+        name='user_follwing',
+    ),
+    url(
+        regex=r'^search/$',
+        view=views.Search.as_view(),
+        name='search',
+    ),
+    url(    
+        regex=r'^(?P<username>\w+)/$',  # ordering to bottom bcs of username 'search'
+        view=views.UserProfile.as_view(),
+        name='user_profile',
     ),
 ]
