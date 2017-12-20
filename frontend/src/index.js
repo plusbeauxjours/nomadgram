55ed23d839fae549128c86c7cfe56a66fde7e967
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-20 22:46:35 +0900

    Changing the NODE_PATH

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'index.css';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import App from './App';
+import store from 'redux/configureStore';
+import App from 'App';
 
 
 console.log(store.getState());  
