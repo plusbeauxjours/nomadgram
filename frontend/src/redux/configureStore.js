9ed080a97b66eb5812775e28012527e977598452
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-25 23:03:49 +0900

    Yarn Added History

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import users from 'redux/modules/users';
 
 const env = process.env.NODE_ENV;
