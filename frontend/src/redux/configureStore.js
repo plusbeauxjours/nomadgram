67de67d9168c21a09364749a59587bcee7ff1de8
plusbeauxjours <plusbeauxjours@gmail.com>
2017-12-27 21:59:56 +0900

    Setting-up ReduxDev-Tools

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,7 +3,9 @@ import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import users from 'redux/modules/users';
-import Reactotron from 'ReactotronConfig';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import Reactotron from 'ReactotronConfig';
 
 const env = process.env.NODE_ENV;
 
@@ -27,7 +29,11 @@ let store;
 
 if(env === 'development'){
     store = initialState => 
-    Reactotron.createStore(reducer, applyMiddleware(...middlewares));  
+    // Reactotron.createStore(
+    createStore(
+        reducer, 
+        composeWithDevTools(applyMiddleware(...middlewares))
+    );  
 } else {
     store = initialState => createStore(reducer, applyMiddleware(...middlewares));  
 }
