ec8d465c9bbb3bdfce0000a8b6a03797de4a6930
plusbeauxjours <plusbeauxjours@gmail.com>
2018-01-29 18:37:36 +0900

    Logout Action

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -93,7 +93,7 @@ class ImageDetail(APIView):
 
         if image is None:
 
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         serializer = serializers.InputImageSerialzier(image, data=request.data, partial=True)
 
@@ -114,7 +114,7 @@ class ImageDetail(APIView):
 
         if image is None:
 
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         image.delete()
 
